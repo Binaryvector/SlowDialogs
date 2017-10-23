7b61e92281dd0e26bfc656f0a76714b0ad2fcdf5
Shinni <shinni@binaryvector.net>
2017-10-23 02:36:34 +0200

    initial commit v1.9.9 CC update

---
+++ SlowDialogs/SlowDialogsOptions.lua
@@ -0,0 +1,56 @@
+local SlowDialogs = SlowDialogsGlobal
+local panelData = {
+	type = "panel",
+	name = "Slow Dialogs",
+	displayName = SlowDialogs.displayName,
+	author = SlowDialogs.author,
+	version = "1.9.9",
+	registerForRefresh = true,
+	registerForDefaults = false,
+}
+
+local optionsTable = {
+	{
+		type = "slider",
+		name = SlowDialogs.delayOption,
+		tooltip = SlowDialogs.delayTooltip,
+		min = 1,
+		step = 1,
+		max = 100,
+		getFunc = function() return SlowDialogs.settings.speed end,
+		setFunc = function(value) SlowDialogs.settings.speed = value end,
+		width = "half",
+		default = 30,
+	},
+	{
+		type = "slider",
+		name = SlowDialogs.startDelayOption,
+		tooltip = SlowDialogs.startDelayTooltip,
+		min = 1,
+		step = 1,
+		max = 1200,
+		getFunc = function() return SlowDialogs.settings.start_delay end,
+		setFunc = function(value) SlowDialogs.settings.start_delay = value end,
+		width = "half",
+		default = 5,
+	},
+	{
+		type = "slider",
+		name = SlowDialogs.fadeLengthOption,
+		tooltip = SlowDialogs.fadeLengthTooltip,
+		min = 1,
+		step = 1,
+		max = 100,
+		getFunc = function() return SlowDialogs.settings.animation_length end,
+		setFunc = function(value) SlowDialogs.settings.animation_length = value end,
+		width = "half",
+		default = 20,
+	},
+}
+
+local LAM = LibStub("LibAddonMenu-2.0")
+
+function SlowDialogsGlobal.InitOptions()
+	LAM:RegisterAddonPanel("SlowDialogsControl", panelData)
+	LAM:RegisterOptionControls("SlowDialogsControl", optionsTable)
+end
